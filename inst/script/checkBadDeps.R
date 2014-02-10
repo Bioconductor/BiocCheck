7751c7b41e1d61b43cc54c243520cc3ed59beb2a
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-02-10 23:00:05 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commit(s).
    Commit information:
    
        Commit id: 55de430e221c35f7f44d6a843f18cf34a8dc8349
        Commit message:
        use dirname instead of sub when deriving lib dir
        Committed by Dan Tenenbaum <dtenenba at fhcrc.org>
        Commit date: 2014-02-10T14:59:23-08:00
    
        Commit id: 488f7d0c5c69b9f1b5540c1ad7a80c49abf53cda
        Commit message:
        Merge branch 'master' of https://github.com/Bioconductor/BiocCheck
        Committed by Dan Tenenbaum <dtenenba at fhcrc.org>
        Commit date: 2014-02-10T14:59:50-08:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocCheck@86300 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/script/checkBadDeps.R
+++ inst/script/checkBadDeps.R
@@ -3,7 +3,8 @@ suppressMessages({
         pkgdir <- commandArgs(TRUE)
         options(useFancyQuotes=FALSE)
         pkgname <- strsplit(basename(pkgdir), "_")[[1]][1]
-        libdir <- sub(pkgname, "", pkgdir)
+        #libdir <- sub(pkgname, "", pkgdir)
+        libdir <- dirname(pkgname)
         .libPaths(c(libdir, .libPaths()))
         library(codetools)
         cat(capture.output(checkUsageEnv(getNamespace(pkgname))), sep="\n")
