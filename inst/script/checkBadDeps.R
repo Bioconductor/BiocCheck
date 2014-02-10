4ebfeaef0c5e5b3246de0b876b62d65ebfea9cc3
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-02-10 23:46:02 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit(s).
    Commit information:
    
        Commit id: 2fafa5e6c0c8ed663a4b14e8d3d5ba3375d6b436
        Commit message:
        Apparently on windows a package must be 'really' installed
        (in binary fashion) before you can call getNamespace() on it.
        Committed by Dan Tenenbaum <dtenenba at fhcrc.org>
        Commit date: 2014-02-10T15:45:21-08:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocCheck@86304 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/script/checkBadDeps.R
+++ inst/script/checkBadDeps.R
@@ -3,8 +3,16 @@ suppressMessages({
         pkgdir <- commandArgs(TRUE)
         options(useFancyQuotes=FALSE)
         pkgname <- strsplit(basename(pkgdir), "_")[[1]][1]
-        #libdir <- sub(pkgname, "", pkgdir)
-        libdir <- dirname(pkgname)
+        if (.Platform$OS.type == "windows")
+        {
+            libdir <- file.path(tempdir(), "libdir")
+            install.packages(pkgname, repos=null, type="source",
+                INSTALL_opts=sprintf("--library=%s",
+                    libdir))
+        } else {
+            libdir <- sub(paste0(pkgname, "$"), "", pkgdir)
+            libdir <- sub("/$|\\$", "", libdir)
+        }
         .libPaths(c(libdir, .libPaths()))
         library(codetools)
         cat(capture.output(checkUsageEnv(getNamespace(pkgname))), sep="\n")
