1ae5c31900c2d1e15ea0cbe4facf1445975f6a08
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-04-07 19:31:57 +0000

    Check for bad deps in the same way on all platforms.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocCheck@88545 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/script/checkBadDeps.R
+++ inst/script/checkBadDeps.R
@@ -3,16 +3,16 @@ suppressMessages({
         pkgdir <- commandArgs(TRUE)
         options(useFancyQuotes=FALSE)
         pkgname <- strsplit(basename(pkgdir), "_")[[1]][1]
-        if (.Platform$OS.type == "windows")
-        {
-            libdir <- file.path(tempdir(), "libdir")
-            install.packages(pkgname, repos=NULL, type="source",
-                INSTALL_opts=sprintf("--library=%s",
-                    libdir))
-        } else {
+        # if (.Platform$OS.type == "windows")
+        # {
+        #     libdir <- file.path(tempdir(), "libdir")
+        #     install.packages(pkgname, repos=NULL, type="source",
+        #         INSTALL_opts=sprintf("--library=%s",
+        #             libdir))
+        # } else {
             libdir <- sub(paste0(pkgname, "$"), "", pkgdir)
             libdir <- sub("/$|\\$", "", libdir)
-        }
+        # }
         .libPaths(c(libdir, .libPaths()))
         library(codetools)
         cat(capture.output(checkUsageEnv(getNamespace(pkgname))), sep="\n")
