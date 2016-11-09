3e0969bfea6bac515f732c15f0775ebae3889420
mtmorgan@fhcrc.org <mtmorgan@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-11-09 11:49:20 +0000

    feature: summarize NOTEs on long lines, etc
    - thanks to Kevin Rue,
      https://stat.ethz.ch/pipermail/bioc-devel/2016-November/010033.html
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocCheck@123807 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/Context-class.R
@@ -0,0 +1,26 @@
+#' Construct a data.frame to summarize location of events
+#'
+#' @param pkg character(1) name of the package
+#' @param file character(1) full path (including package name) of file
+#'     being summarized.
+#' @param lines character() vector of text lines in file
+#' @param idx logical() same length as \code{lines} indicating lines
+#'     in which event occurs
+#'
+#' @return a data.frame() with columns File, Line, and Context
+Context <- function(pkg="", file="", lines=character(), idx=logical()) {
+    data.frame(File=rep(mungeName(file, pkg), sum(idx)), Line=which(idx),
+               Context=lines[idx], stringsAsFactors=FALSE)
+}
+
+#' Report context of events to user
+#'
+#' @param ctxt Object derived from Context()
+#'
+#' @return None; side effect is output on the message stream
+handleContext <- function(ctxt, nlines=6) {
+    ctxt <- head(ctxt, nlines)
+    handleVerbatim(c(
+        sprintf("First %d lines:", nrow(ctxt)),
+        sprintf("%s:%d %s", ctxt$File, ctxt$Line, ctxt$Context)))
+}
