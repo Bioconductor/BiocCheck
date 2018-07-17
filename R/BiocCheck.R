21341d40fd5bcb8c4472b187b29aacccd9d01dbf
LiNk-NY <mramos09@gmail.com>
2018-07-17 16:42:44 -0400

    add quit-with-status option

--- R/BiocCheck.R
+++ R/BiocCheck.R
@@ -9,7 +9,9 @@ getOptionList <- function()
             help="disable biocViews-specific checks (for non-BioC packages)"),
         make_option("--build-output-file", type="character",
             help="file containing R CMD build output, for additional analysis",
-            metavar="build-output-file")
+            metavar="build-output-file"),
+        make_option("--quit-with-status", action="store_true",
+            help="enable exit code option when performing check")
         )
 }
 
@@ -233,7 +235,8 @@ BiocCheck <- function(package=".", ...)
         errcode <- 0
     }
 
-    if (!interactive() || "Called_from_command_line" %in% names(dots))
+    if (isTRUE(dots[["quit-with-status"]]) ||
+        "Called_from_command_line" %in% names(dots))
     {
         q("no", errcode)
     } else {
