909fedc038db2a71cf5693dbd9db13722051efc8
LiNk-NY <marcel.ramosperez@roswellpark.org>
2021-08-12 15:59:13 +0000

    rename and document Context

--- R/Context.R
+++ R/Context.R
@@ -1,24 +1,30 @@
-#' Construct a data.frame to summarize location of events
+#' @rdname Context
+#'
+#' @title Report context of events to user with a data.frame of events and
+#' locations
 #'
 #' @param pkg character(1) name of the package
+#'
 #' @param file character(1) full path (including package name) of file
 #'     being summarized.
+#'
 #' @param lines character() vector of text lines in file
+#'
 #' @param idx logical() same length as \code{lines} indicating lines
 #'     in which event occurs
 #'
-#' @return a data.frame() with columns File, Line, and Context
+#' @return Context: a data.frame() with columns File, Line, and Context
 Context <- function(pkg="", file="", lines=character(), idx=logical()) {
     data.frame(
         File=rep(mungeName(file, pkg), sum(idx)), Line=which(idx),
         Context=lines[idx], stringsAsFactors=FALSE)
 }
 
-#' Report context of events to user
+#' @rdname Context
 #'
 #' @param ctxt Object derived from Context()
 #'
-#' @return None; side effect is output on the message stream
+#' @return handleContext: side effect is output on the message stream
 handleContext <- function(ctxt, nlines=6, width=getOption("width")) {
     ctxt <- head(ctxt, nlines)
     txt <- c(
