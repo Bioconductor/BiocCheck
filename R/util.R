fad89448988b3e8b17a9cd040c56e1b7c0b5ecd5
LiNk-NY <mramos09@gmail.com>
2023-07-17 14:20:37 -0400

    set redownload ask to FALSE when cache is stale

--- R/util.R
+++ R/util.R
@@ -199,7 +199,7 @@ get_status_file_cache <- function(url) {
 
     bquery <- bfcquery(bfc, url, "rname", exact = TRUE)
     if (identical(nrow(bquery), 1L) && bfcneedsupdate(bfc, bquery[["rid"]]))
-        bfcdownload(x = bfc, rid = bquery[["rid"]], rtype = "web")
+        bfcdownload(x = bfc, rid = bquery[["rid"]], rtype = "web", ask = FALSE)
 
     bfcrpath(
         bfc, rnames = url, exact = TRUE, download = TRUE, rtype = "web"
