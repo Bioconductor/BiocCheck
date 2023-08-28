e8c3fc87e19394f2c4aceb33c72dcd69c4f4f6c1
LiNk-NY <marcel.ramos@roswellpark.org>
2023-08-28 16:15:41 -0400

    document offset argument in internal Context()

--- R/Context.R
+++ R/Context.R
@@ -4,10 +4,17 @@
 #'
 #'
 #' @param file character(1) full path (including package name) of file being
-#' summarized.
+#'   summarized.
+#'
 #' @param lines character() vector of text lines in file
+#'
 #' @param idx logical() same length as \code{lines} indicating lines in which
-#' event occurs
+#'   event occurs
+#'
+#' @param offset `integer(1)` The number of lines to add to the 'Line' column
+#'   calculation. It is mainly used to account for the number of lines that the
+#'   YAML header occupies.
+#'
 #' @return Context: a data.frame() with columns File, Line, and Context
 Context <- function(file="", lines=character(), idx=logical(), offset = 0L) {
     data.frame(
